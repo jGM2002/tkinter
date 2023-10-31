6cb2bd81cb5f20c5ab08b55146e0fc45268cb6fe
jGM2002 <josue.matta@ieselcalamot.com>
2023-10-31 15:45:21 +0100

    repositorio actualizado

--- ejercicio32.py
+++ ejercicio32.py
@@ -2,8 +2,39 @@
 import sqlite3
 import tkinter as tk
 from tkinter import messagebox
+import os
+
+directorioBD = 'data'
+nombreBD = 'basquet.db'
+rutaBD = os.path.join(directorioBD, nombreBD)
+# Si la carpeta data no existe, la creara
+if not os.path.exists(directorioBD):
+    os.makedirs(directorioBD)
+# Intentara conectarse a mi base datos y creara la tabla jugadores si no existe
+try:
+    conexionBD = sqlite3.connect(rutaBD)
+    cursorBD = conexionBD.cursor()
+
+    cursorBD.execute("""
+        CREATE TABLE IF NOT EXISTS jugadores (
+            id INTEGER PRIMARY KEY,
+            nombre TEXT,
+            apellido TEXT,
+            altura REAL,
+            edad INTEGER
+        )
+    """)
+
+    conexionBD.commit()
+    print('Base de datos creada exitosamente.')
+except sqlite3.Error as error:
+    print('Error al crear la base de datos:', error)
+finally:
+    if conexionBD:
+        conexionBD.close()
 
 
+# Función que ingresara datos a mi base de datos
 def insertarDatos():
     conexionBD = sqlite3.connect('data/basquet.db')
     cursorDN = conexionBD.cursor()
@@ -59,3 +90,26 @@ entryEdad.grid(row=3, column=1)
 botonInsertar.grid(row=4, column=0, columnspan=2)
 
 ventana.mainloop()
+
+
+# Función que me mostrara el contenido de mi base de datos
+def mostrarContenidoBD():
+    try:
+        conexionBD = sqlite3.connect('data/basquet.db')
+        cursorBD = conexionBD.cursor()
+
+        cursorBD.execute('SELECT * FROM jugadores')
+
+        filas = cursorBD.fetchall()
+
+        for fila in filas:
+            print(fila)
+    except sqlite3.Error as error:
+        print('Error al conectar a la base de datos:', error)
+    finally:
+        if conexionBD:
+            conexionBD.close()
+
+
+input('Presionar Enter para mostrar el contenido de la base de datos...')
+mostrarContenidoBD()
